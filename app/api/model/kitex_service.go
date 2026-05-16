d71ca543043df288eee48c7597db0ef18cabeb68
Lqingzhe <410082155@qq.com>
2026-05-16 18:07:15 +0800

    Delete app directory

--- app/api/model/kitex_service.go
+++
@@ -1,7 +0,0 @@
-package model
-
-import kitexuserservice "aim/kitex_gen/kitexuserservice/userservice"
-
-type ServiceClient struct {
-	UserClient kitexuserservice.Client
-}
--- app/messageservice/tempopary/model/db_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "aim/commonmodel"
-
-type DBContext struct {
-	Mysql   *commonmodel.MysqlContext
-	Redis   *commonmodel.RedisContext
-	MongoDB *commonmodel.MongoDBContext
-}
